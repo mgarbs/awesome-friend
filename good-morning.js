188f02f2c534b71d40eb0979585a0b741a1c01fd
Michael Garber <michaelegarber@gmail.com>
2023-12-15 11:00:16 -0500

    chunk contacts for gpt api call
    Signed-off-by: Michael Garber <michaelegarber@gmail.com>

--- good-morning.js
+++ good-morning.js
@@ -12,7 +12,35 @@ const openai = new OpenAI({
 
 const writeFileAsync = promisify(fs.writeFile);
 const unlinkAsync = promisify(fs.unlink);
+const execAsync = promisify(exec);
 
+// Function to generate messages for a chunk of contacts
+async function generateMessagesForChunk(contactsChunk) {
+    let prompt = "Create a very short, personalized good morning message without any numbers in it. Each message should be no longer than two sentences and have a tone of gratitude and surrender toward God for the following contacts:\n";
+
+    for (const [name, info] of Object.entries(contactsChunk)) {
+        prompt += `- For my ${info.relationship} who likes ${info.interests}, though not necessarily a daily activity. (${info.note ? 'Note: ' + info.note : 'No specific note'})\n`;
+    }
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4-1106-preview',
+        messages: [{ role: 'user', content: prompt }],
+        max_tokens: 60 * Object.keys(contactsChunk).length
+    });
+
+    const generatedMessages = response.choices[0].message.content.trim().split('\n').filter(line => line.trim());
+    let messages = {};
+
+    Object.keys(contactsChunk).forEach((name, index) => {
+        if (index < generatedMessages.length) {
+            messages[name] = generatedMessages[index].replace(/- /g, '');
+        }
+    });
+
+    return messages;
+}
+
+// Function to send text message
 async function sendTextMessage(to, body) {
     const cleanedBody = body.replace(/"/g, ''); // Escape double quotes in the message
     const appleScriptContent = `
@@ -30,43 +58,27 @@ async function sendTextMessage(to, body) {
     } catch (error) {
         console.error(`Error sending message to ${to}: ${error}`);
     } finally {
-        await unlinkAsync(scriptPath); // Clean up the temporary script file
+        await unlinkAsync(scriptPath);
     }
 }
 
-const execAsync = promisify(exec);
-
-// Function to generate messages
-async function generateMessages(contacts) {
-    let prompt = "Create a very short, personalized good morning message without any numbers in it. Each message should be no longer than two sentences and have a tone of gratitude and surrender toward God for the following contacts:\n";
-
-    for (const [name, info] of Object.entries(contacts)) {
-        prompt += `- For my ${info.relationship} who likes ${info.interests}, though not necessarily a daily activity. (${info.note ? 'Note: ' + info.note : 'No specific note'})\n`;
-    }
-
-    const response = await openai.chat.completions.create({
-        model: 'gpt-4-1106-preview',
-        messages: [{ role: 'user', content: prompt }],
-        max_tokens: 60 * Object.keys(contacts).length // Adjusting max tokens based on the number of contacts
-    });
-
-    // Splitting the response into individual messages
-    const generatedMessages = response.choices[0].message.content.trim().split('\n').filter(line => line.trim());
+// Function to process contacts in chunks
+async function processContactsInChunks(contacts) {
+    const chunkSize = 20;
     let messages = {};
 
-    // Associating each message with a contact
-    Object.keys(contacts).forEach((name, index) => {
-        if (index < generatedMessages.length) {
-            messages[name] = generatedMessages[index].replace(/- /g, '');
-        }
-    });
+    for (let i = 0; i < contacts.length; i += chunkSize) {
+        const chunk = Object.fromEntries(Object.entries(contacts).slice(i, i + chunkSize));
+        const chunkMessages = await generateMessagesForChunk(chunk);
+        messages = { ...messages, ...chunkMessages };
+    }
 
     return messages;
 }
 
 // Main function
 async function main() {
-    const messages = await generateMessages(contacts);
+    const messages = await processContactsInChunks(contacts);
 
     for (let [name, message] of Object.entries(messages)) {
         await sendTextMessage(contacts[name].phone, message);
